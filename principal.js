bd35ba7d528ea78c3a8c1c044040428081ddaaa7
Arano Orkatz <oarano@lekooa.com>
2017-11-25 09:54:31 +0100

    create play image

--- principal.js
+++ principal.js
@@ -8,21 +8,21 @@ var estadoPrincipal = {
     preload: function () {
         // carga todos los recursos
         juego.load.image('fondo', 'img/bg.jpeg');
-        juego.load.image('pajaro', 'img/pajaro1.png');
+        juego.load.image('wario', 'img/wario.png');
         juego.load.image('btn', 'img/btn.png')
     },
 
     create: function () {
         // mostrar en pantalla
         fondoJuego = juego.add.tileSprite(0, 0, 370, 550, 'fondo');
-        // juego.add.sprite(0, 0, 'pajaro'); // sprite = image
+        // juego.add.sprite(0, 0, 'wario'); // sprite = image
         boton = juego.add.sprite(juego.width / 2, juego.height / 2, 'btn');
         boton.anchor.setTo(0.5);
     },
 
     update: function () {
         // Animamos el juego
-        fondoJuego.tilePosition.x -= 1;
+        // fondoJuego.tilePosition.x -= 1;
     }
 };
 
