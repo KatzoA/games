5bfd5085863c64ddd3ee7d2696a8dab044123a51
Arano Orkatz <oarano@lekooa.com>
2017-11-26 20:05:49 +0100

    display wario in center

--- principal.js
+++ principal.js
@@ -1,5 +1,6 @@
 var fondoJuego;
 var boton;
+var flappy;
 
 var juego = new Phaser.Game(370, 550, Phaser.CANVAS, 'bloque_juego');
 
@@ -9,15 +10,17 @@ var estadoPrincipal = {
         // carga todos los recursos
         juego.load.image('fondo', 'img/bg.jpeg');
         juego.load.image('wario', 'img/wario.png');
-        juego.load.image('btn', 'img/btn.png')
+        // juego.load.image('btn', 'img/btn.png')
     },
 
     create: function () {
         // mostrar en pantalla
         fondoJuego = juego.add.tileSprite(0, 0, 370, 550, 'fondo');
         // juego.add.sprite(0, 0, 'wario'); // sprite = image
-        boton = juego.add.sprite(juego.width / 2, juego.height / 2, 'btn');
-        boton.anchor.setTo(0.5);
+        // boton = juego.add.sprite(juego.width / 2, juego.height / 2, 'btn');
+        flappy = juego.add.sprite(juego.width / 2, juego.height / 2, 'wario');
+        flappy.anchor.setTo(0.5);
+        flappy.scale.setTo(1, 1);
     },
 
     update: function () {
